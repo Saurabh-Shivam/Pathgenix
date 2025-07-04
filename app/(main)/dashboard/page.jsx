41e3d1a800509fb11438b43223813437df8ba081
Saurabh Shivam <saurabhshivam04@gmail.com>
2025-07-04 14:27:13 +0530

    created the dashboard page by fetching data from AI

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,5 +1,5 @@
 import { getIndustryInsights } from "@/actions/dashboard";
-// import DashboardView from "./_component/dashboard-view";
+import DashboardView from "./_component/dashboard-view";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
 
@@ -16,8 +16,7 @@ export default async function DashboardPage() {
 
   return (
     <div className="container mx-auto">
-      {/* <DashboardView insights={insights} /> */}
-      hey
+      <DashboardView insights={insights} />
     </div>
   );
 }
